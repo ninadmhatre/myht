6fc8e0ec3856101ed6410ee418bb336725dccb62
ninadmhatre <ninad.mhatre@gmail.com>
2020-12-20 00:51:43 +0100

    [v0.0.8] add missing files + change in gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,1 +1,9 @@
-gunicorn -w4 application:app -k meinheld.gmeinheld.MeinheldWorker
+#! /bin/bash
+
+. /home/stuser/venv_py37_st/bin/activate
+
+gunicorn --workers 3 \
+       	application:app \
+	      -k meinheld.gmeinheld.MeinheldWorker \
+	      --bind unix:stweb.sock \
+        -m 007
